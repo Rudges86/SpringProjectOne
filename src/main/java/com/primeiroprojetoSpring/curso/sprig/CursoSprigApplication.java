7b37230dd085dc55c40bbd2797891881eda11e98
Rudge Santos <rudges86@gmail.com>
2022-12-13 19:09:07 -0300

    Projeto Criado

---
+++ src/main/java/com/primeiroprojetoSpring/curso/sprig/CursoSprigApplication.java
@@ -0,0 +1,13 @@
+package com.primeiroprojetoSpring.curso.sprig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSprigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSprigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primeiroprojetoSpring/curso/sprig/CursoSprigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeiroprojetoSpring.curso.sprig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSprigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
