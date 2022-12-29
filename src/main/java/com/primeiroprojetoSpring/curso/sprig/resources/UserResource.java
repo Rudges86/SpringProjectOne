ad2eb4fb0f04ddabb06508600d805d5d1c13d672
Rudge Santos <rudges86@gmail.com>
2022-12-29 16:30:50 -0300

    feito o insert do user, e colocado o status 201

--- src/main/java/com/primeiroprojetoSpring/curso/sprig/resources/UserResource.java
+++ src/main/java/com/primeiroprojetoSpring/curso/sprig/resources/UserResource.java
@@ -6,7 +6,9 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,17 +18,28 @@ public class UserResource {
     private Userservice service;
 
     @GetMapping
-    @ResponseBody
     public ResponseEntity<List<User>> findAll(){
     List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping("/{id}")
-    @ResponseBody
     public ResponseEntity<User> findById(@PathVariable Long id){
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    //para retornar um created no ResponseEnttity, quando utilizamos um ResponseEntity.created(), para retornarmos um 201
+    //É esperado que ele retorne um cabeçalho chamado location com o enderço do novo recurso que você inseriu
+    //Ai no springboot temos uma forma padrão de fazer isso
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        user = service.insert(user);
+        //forma padrão de retorno
+        //path, passe o caminho, buildAndExpand -> passe o id, no final passe o toUri() para ele converter para o URI
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(user);
     }
 
 }
--- src/main/java/com/primeiroprojetoSpring/curso/sprig/service/Userservice.java
+++ src/main/java/com/primeiroprojetoSpring/curso/sprig/service/Userservice.java
@@ -25,4 +25,8 @@ public class Userservice {
         return find.get();
     }
 
+    public User insert (User obj){
+       return userRepository.save(obj);
+    }
+
 }
