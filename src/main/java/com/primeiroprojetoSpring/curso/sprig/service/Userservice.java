2910937fe3f2e760ebda7df8a21deb30b00e88b8
Rudge Santos <rudges86@gmail.com>
2022-12-29 16:52:13 -0300

    Adicionado o método de deletar, e de atualizar usuário

--- src/main/java/com/primeiroprojetoSpring/curso/sprig/service/Userservice.java
+++ src/main/java/com/primeiroprojetoSpring/curso/sprig/service/Userservice.java
@@ -33,4 +33,17 @@ public class Userservice {
         userRepository.deleteById(id);
     }
 
+
+    public User update (Long id, User obj) {
+        User entity = userRepository.getReferenceById(id);
+        updateData(entity,obj);
+        return userRepository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setNome(obj.getNome());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
+
 }
